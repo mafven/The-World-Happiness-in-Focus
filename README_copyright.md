30964a48e8f8b36477c21bf3ef2e8a6f0c6bacc3
unknown <maravenables@gmail.com>
2021-05-31 22:38:54 +0800

    update plot1

--- README_copyright.md
+++ README_copyright.md
@@ -4,3 +4,8 @@ The original home page template was obtained from StartBootstrap as shown below.
 
 ## Copyright and License
 Copyright 2013-2021 Start Bootstrap LLC. Code released under the [MIT](https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE) license.
+
+
+Note - Oxford Happiness Questionnaire was "Developed by Michael Argyle and Peter Hills of Oxford Brookes University, and originally published in 2002 in the Journal of Personality and Individual Differences, it’s one several measures of “subjective wellbeing” (aka happiness) constructed by scientific research psychologists".
+
+ref.:https://www.theguardian.com/lifeandstyle/2014/nov/03/take-the-oxford-happiness-questionnaire
--- static/js/plot1.js
+++ static/js/plot1.js
@@ -1,173 +1,180 @@
+// JavaScript Questionnaire for id="Questionnaire" - plot1.html
+// select  HTML elements and store 
+//functions to buid questionaire and show users results
+function buildQuestionnaire(){
+  // variable to store the HTML output
+  const output = [];
 
-const api_url_main = '/api/main'
+  // for each question...
+  Questionnaire.forEach(
+    (currentQuestion, questionNumber) => {
 
-async function getData_main(){
-const response_main = await fetch(api_url_main)
-const data_main = await response_main.json();
-console.log(data_main)
+      // variable to store the list of possible answers
+      const answers = [];
 
-let xl = []
-let yl = []
-let Country = []
-for (var i=0; i< data_main.length; i++){
-xl.push(data_main[i]['year'])
-yl.push(data_main[i]['Life_ladder'])
-Country.push(data_main[i]['Country']) }
+      // and for each available answer...
+      for(letter in currentQuestion.answers){
 
-var data = [{
-  type: 'choropleth',
-  locationmode: 'country names',
-  locations: Country,
-  z: yl,
-  text: Country,
-  transforms: [{
-    type: 'aggregate',
-    groups: Country,
-    aggregations: [
-      {target: 'z', func: 'avg', enabled: true},
-    ]
-  }],
-    colorscale: [
-      [0,'#F9F871'],[0.35,'#FFC75F'],
-      [0.5,'#FF9671'], [0.6,'#FF6F91'],
-      [0.7,'#D65DB1'],[1,'#845EC2']],
-  autocolorscale: false,
-  reversescale: true,
-  marker: {
-      line: {
-          color: 'rgb(180,180,180)',
-          width: 0.3
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
       }
-  },
-  tick0: 0,
-  zmin: 0,
-  dtick: 1000,
-  colorbar: {
-      autotic: false,
-      tickprefix: '',
-      title: 'Life<br>Ladder'
-  }
-}];
 
-var layout = {
-title: 'Average Happiness by Countries, 2008 - 2020',
-geo:{
-    showframe: false,
-    showcoastlines: false,
-    height: 600,
-    width: 900,
-    projection:{
-    type: 'mercator'
-    },
-     margin: {l: 0,r: 0,b: 0,t: 0,pad: 1 }
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+      );
+    }
+  );
+
+  // finally combine our output list into one string of HTML and put it on the page
+  QuestionnaireContainer.innerHTML = output.join('');
 }
-};
-Plotly.newPlot("main", data, layout, {showLink: false});
 
-}
-getData_main();
 
-const api_url = '/api/main'
-async function getData(){
-const response = await fetch(api_url)
-const data = await response.json();
-console.log(data)
-let xl = []
-let yl = []
-let y2 = []
-let y3 = []
-let y4 = []
-let y5 = []
-let y6 = []
-let label = []
-for (var i=0; i< data.length; i++){
-  if (data[i]['Life_ladder'] <= 3){
-  xl.push(data[i]['year'])
-  yl.push(data[i]['Life_ladder'])
-  y2.push(data[i]['GDP_per_capita'])
-  y3.push(data[i]['Life_expectancy'])
-  y4.push(data[i]['Social_support'])
-  y5.push(data[i]['Corruption'])
-  y6.push(data[i]['Freedom'])
-  label.push(data[i]['Country']) }
+function showResults(){
 
-  xl = xl.slice(0,10).reverse()
-  yl =  yl.slice(0,10).reverse()
-  label =  label.slice(0,10).reverse()
+  // gather answer containers from our quiz
+  const answerContainers = QuestionnaireContainer.querySelectorAll('.answers');
 
-var trace1 = {
-x: label,
-y: yl,
-    text:xl,
-    type: 'bar',
-    // orientation:"h",
-    name: 'Life_ladder',
-    marker: {
-      color: '#845EC2',
-      opacity: 0.8,
+  // keep track of user's answers
+  let numCorrect = 0;
+  
+
+  // for each question...
+  Questionnaire.forEach( (currentQuestion, questionNumber) => {
+
+    // find selected answer
+    const answerContainer = answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    // if answer is correct
+    if(userAnswer === currentQuestion.correctAnswer){
+      // add to the number of correct answers
+      numCorrect++;
+
+      // color the answers green
+      answerContainers[questionNumber].style.color = 'lightgreen';
+    }
+    // if answer is wrong or blank
+    else{
+      // color the answers red
+      answerContainers[questionNumber].style.color = 'red';
+    }
+  });
+
+  // apply if conditions for the answers
+    if(numCorrect <=2){
+      // add to the number of correct answers
+      // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${Questionnaire.length}
+    Not happy. You may be seeing this project as worse than it really is. Try to xxxxxx `;
+    }
+
+    if(3 < numCorrect && numCorrect<= 4){
+      // add to the number of correct answers
+      // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${Questionnaire.length} ---
+    Neutral – not really happy or unhappy. Xxxxx xxxxx xxxxx `;
+    }
+    if(numCorrect >=5){
+      // add to the number of correct answers
+      // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${Questionnaire.length}
+    Extremely happy. That’s right – it’s possible to be too happy. xxxxxxx xxxxxx .`;
     }
   };
 
-  var data1 = [trace1];
-  var layout = {
-    title: 'Title',
-    width:600, 
-    height:400,
-    xaxis: {
-    tickangle: -45
+
+// Variables
+const QuestionnaireContainer = document.getElementById('Questionnaire');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+// Questionnaire based on the The Oxford Happiness Questionnaire  ref. readMe
+const Questionnaire = [
+  {
+    question: "I don't feel particularly pleased with the way this website is.",
+    answers: {
+      a: "Agree",
+      b: "Disagree",
     },
-    barmode: 'group'
-  };
+    correctAnswer: "b"
+  },
 
-  var data2 = [{
-    type: 'scatter',
-    mode: 'markers',
-    x: yl,
-    y: y2,
-    text:  label,
-    marker: {
-      size:  yl*20000000,
-      sizemode: "area",
+  {
+    question: "I feel that this subject is very interesting.",
+    answers: {
+      a: "Agree",
+      b: "Disagree",
     },
-    transforms: [
-      {
-      type: 'filter',
-      target: yl,
-      operation: '<=',
-      value: '3'
-      }, {
-      type: 'groupby',
-      groups:  xl,
-      styles: [
-        {target: '2008', value: {marker: {color: 'red'}}},
-        {target: '2009', value: {marker: {color: 'blue'}}},
-        {target: '2010', value: {marker: {color: 'orange'}}},
-        {target: '2011', value: {marker: {color: 'green'}}},
-        {target: '2012', value: {marker: {color: 'green'}}},
-        {target: '2013', value: {marker: {color: 'green'}}},
-        {target: '2014', value: {marker: {color: 'green'}}},
-        {target: '2016', value: {marker: {color: 'green'}}},
-        {target: '2017', value: {marker: {color: 'green'}}},
-        {target: '2018', value: {marker: {color: 'green'}}},
-        {target: '2019', value: {marker: {color: 'green'}}},
-        {target: '2020', value: {marker: {color: 'purple'}}}
-      ]
-}]
-  }]
+    correctAnswer: "a"
+  },
+  {
+    question: "I find most things about this project amusing.",
+    answers: {
+      a: "Agree",
+      b: "Disagree",
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "I do not think that the world is a happy place.",
+    answers: {
+      a: "Agree",
+      b: "Disagree",
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "I laugh a lot.",
+    answers: {
+      a: "Agree",
+      b: "Disagree",
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "I am well satisfied about everything in my life ( and with this project).",
+    answers: {
+      a: "Agree",
+      b: "Disagree",
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "There is a gap between what I would like to do and what I have done if I was doing this project.",
+    answers: {
+      a: "Agree",
+      b: "Disagree",
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "I am very happy.",
+    answers: {
+      a: "Agree",
+      b: "Disagree",
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "I find beauty in some things (inclusive on this website).",
+    answers: {
+      a: "Agree",
+      b: "Disagree",
+    },
+    correctAnswer: "a"
+  },
+];
 
-var layout = {
-yaxis: {
-  type: 'log'
-}
-}
-  
-  Plotly.newPlot('plot1', data1, layout);
-  Plotly.newPlot('plot2', data2, layout);
-  // Plotly.newPlot('plot3', data3, layout);
-  // Plotly.newPlot('plot4', data4, layout);
-  // Plotly.newPlot('plot5', data5, layout)
-
-}
-}
-getData();
-
+buildQuestionnaire()
+// Event listeners
+submitButton.addEventListener('click', showResults)
